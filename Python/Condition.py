90c23d7a764718d9840c3e240d61054d78bed9c3
CHHyuk <gksgur1038@naver.com>
2022-10-20 22:17:52 +0900

    파이썬2

---
+++ Python/Condition.py
@@ -0,0 +1,20 @@
+0 < 10 # 초과
+4 > 10 # 미만
+3 <= 10 # 이상
+15 >= 10 # 이하
+5 == 5 # 같다면 True
+10 != 5 # 다르면 False
+
+
+# boolean 연산자
+
+True and True #and는 두 조건이 True인 경우 True, 하나라도 False인 경우 False
+True and False
+False and True
+False and False
+True or True #or는 둘 중 하나라도 True면 True, 둘 다 False라면 False
+True or False
+False or True
+False or False
+not True #not은 True를 False로, False를 True로 변환
+not False
