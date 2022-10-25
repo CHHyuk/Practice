9caf1d2ed80edf620879c2b504d3130ec4b379bf
CHHyuk <gksgur1038@naver.com>
2022-10-25 23:58:43 +0900

    123

--- Python/MyProject/calculator_1for.py
+++ Python/MyProject/calculator_1for.py
@@ -13,8 +13,20 @@ def divide(a,b):
 my_list = input(str)
 list = list(my_list)
 
-result = 0
+result = int(0)
 temp_symbol = ['+','-','*','/']
 
 for index, x in enumerate(list):
-    if 
+    if index == 0:
+        result = x          
+    elif x in temp_symbol:
+        if x == '+':
+            result = add(int(result),int(list[index+1]))
+        elif x == '-':
+            result = minus(int(result),int(list[index+1]))
+        elif x == '*':
+            result = multiply(int(result),int(list[index+1]))
+        elif x == '/':
+            result = divide(int(result),int(list[index+1]))
+
+print(result)
