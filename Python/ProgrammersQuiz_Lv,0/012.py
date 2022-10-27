167b0a3df3af594c3396e2dc8041b81883222c26
CHHyuk <gksgur1038@naver.com>
2022-10-27 22:34:36 +0900

    10/27 퀴즈

--- Python/ProgrammersQuiz_Lv,0/012.py
+++ Python/ProgrammersQuiz_Lv,0/012.py
@@ -4,3 +4,11 @@ Lv.0 머쓱이보다 키 큰 사람
 머쓱이네 반 친구들의 키가 담긴 정수 배열 array와 머쓱이의 키 height가 매개변수로 주어질 때, 
 머쓱이보다 키 큰 사람 수를 return 하도록 solution 함수를 완성해보세요.
 """
+
+
+def solution(array, height):
+    answer = 0
+    for i in array:
+        if i > height:
+            answer += 1
+    return answer
