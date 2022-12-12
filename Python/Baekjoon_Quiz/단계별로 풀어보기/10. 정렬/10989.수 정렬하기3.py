ad68879d2b183c42f4d9c69c415e99f0fbf6c973
CHHyuk <gksgur1038@naver.com>
2022-12-12 22:15:53 +0900

    12/12 문제풀이

---
+++ Python/Baekjoon_Quiz/단계별로 풀어보기/10. 정렬/10989.수 정렬하기3.py
@@ -0,0 +1,13 @@
+"""
+N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+"""
+
+n = int(input())
+array = []
+
+for _ in range(n):
+    array.append(int(input()))
+
+array.sort()
+for i in range(len(array)):
+    print(array[i])
